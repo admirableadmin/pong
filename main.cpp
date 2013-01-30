681a9ddf4457fc7f0149f189c939620c3bf93154
Andreas Peichert <peichert@gmail.com>
2013-01-30 16:07:04 +0100

    include pong files

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
 /*
  * File:   main.cpp
- * Author: ap
+ * Author: Andreas Peichert <peichert@gmail.com>
  *
  * Created on January 30, 2013, 3:08 PM
  */
 
-#include <QtGui/QApplication>
+#include <QApplication>
+#include <QWidget>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+#include "pong.h"
 
 int main(int argc, char *argv[]) {
-    // initialize resources, if needed
-    // Q_INIT_RESOURCE(resfile);
-
     QApplication app(argc, argv);
+    QWidget *main = new QWidget();
 
-    // create and show your widgets here
+    QGraphicsScene *scn = new Pong();
+    QGraphicsView *view = new QGraphicsView(scn, main);
 
+    main->resize(scn->width(), scn->height());
+    main->show();
     return app.exec();
 }
